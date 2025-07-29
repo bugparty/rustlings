abd93183d7bba40c83395047fc05e15e73814fd0
bowman <fancycode@gmail.com>
2025-07-29 00:14:00 -0700

    Remove unnecessary blank line in result_with_list function

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -26,7 +26,6 @@ fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
 // Desired output: `Ok([1, 11, 1426, 3])`
 fn result_with_list()-> Result< Vec<i64>, DivisionError> {
     let numbers = [27, 297, 38502, 81];
-
     let division_results :Vec<i64> = numbers.into_iter()
     .map(|n| n / 27)
     .collect();
